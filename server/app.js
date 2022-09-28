9802547ecadb84d8697ce46a6c653c721a01cd93
oryx <mtxymtxy@gmail.com>
2022-09-28 02:24:05 -0400

    server now serves the react build

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
@@ -10,8 +11,10 @@ const io = new Server(server,{
     }
   }); 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+app.use(express.static(path.resolve(__dirname, '../dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
 })
 
 io.on('connection', (socket) => {
