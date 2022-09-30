e1e860f590365fc9ef9c2bd9c50972eca13ff350
oryx <mtxymtxy@gmail.com>
2022-09-29 23:30:29 -0400

    add circle component

---
+++ src/components/Circle.jsx
@@ -0,0 +1,7 @@
+export default function Circle({ size, color }) {
+    return(
+        <svg width={size} height={size} viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="32" cy="32" r="16" stroke={color} stroke-width="5"/>
+        </svg>
+    )
+}
